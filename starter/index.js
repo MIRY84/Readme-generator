8686342e7bfd4dec246b8cd0a17d16be83ec98eb
Mirella <mirella.dimichino@gmail.com>
2023-02-19 12:20:55 +0000

    added input questions/messages for users index.js

--- starter/index.js
+++ starter/index.js
@@ -4,7 +4,48 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = [{
+    type:'input',
+    name: 'Project Title',
+    message:'What is your project title?'
+},
+{
+    type:'input',
+    name:'Description',
+    message: 'What is your project about?'
+},
+{
+    type:'input',
+    name:'Installation',
+    message:'How do you install your project?'
+},
+{
+    type: 'input',
+    name: 'Usage',
+    message:'How do you use it?'
+},
+{
+    type:'checkbox',
+    name: 'License',
+    message:'Which licence are you applying for?',
+    choices: ['MIT', 'Apache', 'Mozilla', 'none']
+},
+{
+    type:'input',
+    name:'Contributors',
+    mesage:'Please list any contributors for the project'
+
+},
+{
+    type:'input',
+    name:'Tests',
+    message:'Please link live url'
+},
+{
+    type:'input',
+    name: 'Questions',
+    message:'Please insert you email address and github link'
+}
 
 ];
 
